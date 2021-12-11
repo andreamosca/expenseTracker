ef8caf5660775054d72ecb0c53d7f92576453fba
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-11 14:11:33 +0100

    STATE MANAGEMENT

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "src/components/UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [sTitle, setTitle] = useState(props.title);
+  const handleClick = () => {
+    setTitle("Aggiornato");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{sTitle}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
+      <button onClick={handleClick}>Cambia titolo</button>
     </Card>
   );
 }
