cf0f0bdf42b6b1a2f8f97fa360cb661d1124b1e8
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-14 18:22:45 +0100

    Aggiunta component Stateless (ExpensesFilter), gestione dello state al padre Stateful (Expenses)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "src/components/UI/Card";
 
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [year, setYear] = useState("2022");
+  const handleYearSelection = (selectedYear) => {
+    setYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter year={year} onYearSelection={handleYearSelection} />
       {props.items.map((prop) => {
-        return (<ExpenseItem key={prop.id} title={prop.title} amount={prop.amount} date={prop.date} />);
-      })} 
-      </Card>
+        return (
+          <ExpenseItem
+            key={prop.id}
+            title={prop.title}
+            amount={prop.amount}
+            date={prop.date}
+          />
+        );
+      })}
+    </Card>
   );
-}
+};
 
 export default Expenses;
