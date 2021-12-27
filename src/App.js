b14510bcc0998d82a75cdd738240c79b720a7cb9
andreamosca <andrea.mosca.prod@gmail.com>
2021-12-27 11:30:05 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import React, { useState } from "react";
 import NewExpense from "src/components/NewExpense/NewExpense";
 import Expenses from "src/components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "1",
+    title: "Meccanico Moto",
+    amount: 250,
+    date: new Date(2021, 10, 31),
+  },
+  { id: "2", title: "Panino", amount: 10, date: new Date(2021, 12, 7) },
+  { id: "3", title: "Computer", amount: 1000, date: new Date(2021, 11, 20) },
+  { id: "4", title: "Scarpe", amount: 130, date: new Date() },
+];
+
 const App = () => {
 
-  const [aExpenses, setExpenses] = useState([
-    {
-      id: "1",
-      title: "Meccanico Moto",
-      amount: 250,
-      date: new Date(2021, 10, 31),
-    },
-    { id: "2", title: "Panino", amount: 10, date: new Date(2021, 12, 7) },
-    { id: "3", title: "Computer", amount: 1000, date: new Date(2021, 11, 20) },
-    { id: "4", title: "Scarpe", amount: 130, date: new Date() },
-  ]);
+  const [aExpenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const handleAddExpense = oExpense => {
-    setExpenses((prev) => {
-      return [...prev, oExpense];
-    });
-  };
+  const handleAddExpense = expense => setExpenses((prev) => [expense, ...prev]);
+  
   return (
     <div>
       <NewExpense onAddExpense={handleAddExpense} />
